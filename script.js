a8d76d5f1a33cfe15bd7b8d946f3e60fae4de90e
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-08 22:39:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+	let result = '';
+	const char = 'QWERTYUIOPLKJHGFDSAZXCVBNMqwertyuioplkjhgfdsazxcvbnm0123456789';
+	const charLength = char.length;
+	for (let i = 0; i < l; i++) {
+		const random = Math.floor(Math.random() * charLength); 
+		result += char.charAt(randomIndex); 
+	}
+	return result;
 }
 
 // Do not change the code below.
